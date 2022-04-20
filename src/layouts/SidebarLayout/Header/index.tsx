861c7e296be32115ab4de5cfc7296c4e848f58cb
MuhammadUmar00 <darazkhan250@gmail.com>
2022-04-20 15:24:59 +0500

    fix: the website icon now matches with the figma design

--- src/layouts/SidebarLayout/Header/index.tsx
+++ src/layouts/SidebarLayout/Header/index.tsx
@@ -1,12 +1,16 @@
 import { useContext } from 'react';
 
-import { Box } from '@mui/material';
+import { Box, Hidden, IconButton, Tooltip } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { SidebarContext } from 'src/contexts/SidebarContext';
 
 import HeaderButtons from './Buttons';
 import HeaderUserbox from './Userbox';
 import HeaderAction from './Action';
+import HeaderMenu from './Menu';
+
+import MenuTwoToneIcon from '@mui/icons-material/MenuTwoTone';
+import CloseTwoToneIcon from '@mui/icons-material/CloseTwoTone';
 
 const HeaderWrapper = styled(Box)(
   ({ theme }) => `
@@ -25,9 +29,20 @@ function Header() {
 
   return (
     <HeaderWrapper >
-      <Box display="flex" justifyContent={'flex-end'} alignItems="center" style={{marginTop:10}}>
+      <Box display="flex" justifyContent={'space-between'} alignItems="center">
+      <Box display="flex" justifyContent={'flex-start'} alignItems="center" style={{ marginTop: 10 }}>
+        <Hidden lgUp>
+          <Tooltip arrow title="Toggle Menu">
+            <IconButton color="primary" onClick={toggleSidebar}>
+              {!sidebarToggle ? <img src='https://test-champria.herokuapp.com/icons/menu-burger.svg' /> : <CloseTwoToneIcon />}
+            </IconButton>
+          </Tooltip>
+        </Hidden>
+      </Box>
+      <Box display="flex" justifyContent={'flex-end'} alignItems="center" style={{ marginTop: 10 }}>
         <HeaderButtons />
         <HeaderUserbox />
+      </Box>
       </Box>
       <HeaderAction />
     </HeaderWrapper>
